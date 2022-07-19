8042a42586dee2ee2ee52904fc1a2f4c8e16a48b
Julien Armagnac <julien.tamil@gmail.com>
2022-07-19 15:18:17 +0200

    Add a basic design.

--- javascript/main.js
+++ javascript/main.js
@@ -9,14 +9,16 @@ const getArticlesTryCatch = async () => {
     try {
         const response = await fetch(req);
         const data = await response.json();
-        console.log(data.articles);
-        getArticleProperties(data.articles[0]);
+        displayArticles(data.articles);
+
     } catch (error) {
         console.error('Problème de requête asynchrone : ' + error);
     }
 }
 
 getArticlesTryCatch();
+
+// FIN
 
 // 2e façon de faire de l'asynchorne
 const getArticlesThenCatch = async () => {
@@ -28,8 +30,53 @@ const getArticlesThenCatch = async () => {
     
 getArticlesThenCatch();
 
-getArticleProperties = (article) => {
-    console.log(Object.keys(article));
-}
+// FIN
 
-    
+
+
+// Affichage des articles de la 1ère méthode
+displayArticles = (articlesArray) => {
+    let html = '';
+    const properties = getArticleProperties(articlesArray[0]);
+    // console.log(properties);
+
+    articlesArray.map(article => {
+        // Extraction des informations dans le but de les formatter en HTML.
+        let source = article.source.name;
+        let author = article.author;
+        let title = article.title;
+        let description = article.description;
+        let url = article.url;
+        let imageURL = article.urlToImage;
+        let publishingDate = article.publishedAt;
+        let content = article.content;
+        console.log(source, author, title, description, url, imageURL, publishingDate, content);
+
+        // Création de la structure HTML pour afficher ces informations.
+        let htmlSegment =`
+        <div class="news-container align-items-center">
+            <div class="image-container">
+                <div class="p-3">
+                    ${article.title}
+                </div>
+                <div>
+                    <img src="${article.urlToImage}" alt="image" class="rounded-pill"/>
+                </div>       
+            </div>
+
+            <div class="text-container">
+                <p class="p-3">${article.content}<br/>--> <a href=\"${article.url}\" >read more</a></p>
+                <p class="fs-6 fw-medium p-3" >by ${article.author} [${article.source.name}] | on ${article.publishedAt}</p>
+            </div>
+        </div>
+            `
+        html += htmlSegment;
+    });
+
+    const mainContainer = document.getElementById('main-container');
+    mainContainer.innerHTML = html;
+} 
+
+getArticleProperties = (article) => {
+    return Object.keys(article);
+}
